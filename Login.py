54ab9dd35e69b2f34834dcbbe659c9b419cd7c8f
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2025-04-07 19:59:51 +0700

    temp login

--- Login.py
+++ Login.py
@@ -1,29 +1,53 @@
 import streamlit as st
 from supabase import create_client
-import services.auth as auth
+from streamlit_url_fragment import get_fragment
+from streamlit_cookies_controller import CookieController
 
-# Supabase client
 SUPABASE_URL = st.secrets["supabase"]["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["supabase"]["SUPABASE_KEY"]
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-st.title("🔐 Login with Google")
+cookie_manager = CookieController()
 
-if st.button("Sign in with Google"):
-    response = supabase.auth.sign_in_with_oauth({
-        "provider": "google",
-        "options": {
-            "redirect_to": "http://localhost:8501"
-        }
-    })
-    st.markdown(f"""
-    <meta http-equiv="refresh" content="0;url={response.url}" />
-    <script>window.location.replace("{response.url}");</script>
-    """, unsafe_allow_html=True)
+def extract_access_token_from_fragment():
+    fragment = get_fragment()
+    if not fragment:
+        return None
+    params = dict(x.split("=") for x in fragment.split("&") if "=" in x)
+    return params.get("#access_token")
 
-access_token = auth.extract_access_token_from_fragment()
-access_token
+def main():
 
-auth.set_access_token_cookie(access_token)
 
-st.write(auth.get_access_token_from_cookie())
+    st.title("🔐 Google Login")
+
+    if st.button("Sign in with Google"):
+        response = supabase.auth.sign_in_with_oauth({
+            "provider": "google",
+            "options": {
+                "redirect_to": "http://localhost:8501"
+            }
+        })
+        st.markdown(f"""
+        <meta http-equiv="refresh" content="0;url={response.url}" />
+        <script>window.location.replace("{response.url}");</script>
+        """, unsafe_allow_html=True)
+
+    fragment = get_fragment()
+    if not fragment:
+        return None
+    params = dict(x.split("=") for x in fragment.split("&") if "=" in x)
+
+    access_token = params.get("#access_token")
+
+    if access_token:
+        cookie_manager.set("access_token", access_token)
+
+    token = cookie_manager.get("access_token")
+    st.write(token)
+
+    user = supabase.auth.get_user(token)
+    st.write(user)
+    
+if __name__ == "__main__":
+    main()
